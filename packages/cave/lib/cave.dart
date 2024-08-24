636d172fc6f9a06a4ca0c52ac99d0f2bbd022daa
Codefarmer <abadasamuelosp@gmail.com>
2024-08-24 18:14:39 +0100

    💄 Chore: setup widget catalog

--- packages/cave/lib/cave.dart
+++ packages/cave/lib/cave.dart
@@ -1,7 +1,4 @@
 library cave;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'themes/themes.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart' hide DeviceType;
---
+++ packages/cave/lib/themes/themes.dart
@@ -0,0 +1,5 @@
+export 'colors.dart';
+export 'effects.dart';
+export 'theme_data.dart';
+export 'theme_widget.dart';
+export 'typography.dart';
