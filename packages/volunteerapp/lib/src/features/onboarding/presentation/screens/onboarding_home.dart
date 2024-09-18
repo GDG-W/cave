33f24908ea229be5efc8b69b926623e79b3de5dd
Iyinoluwa <dairoiyin@gmail.com>
2024-09-18 15:43:27 +0100

    chore: added images onboarding page

--- packages/volunteerapp/lib/src/features/onboarding/presentation/screens/onboarding_home.dart
+++ packages/volunteerapp/lib/src/features/onboarding/presentation/screens/onboarding_home.dart
@@ -1,6 +1,7 @@
 import 'package:cave/cave.dart';
 import 'package:cave/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:volunteerapp/src/routing/routing.dart';
 import 'package:volunteerapp/src/shared/shared.dart';
@@ -11,23 +12,42 @@ class OnboardingHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+          leading: Padding(
+              padding: EdgeInsets.only(left: Constants.horizontalMargin.w),
+              child: SvgPicture.asset(
+                'assets/icons/Logo.svg',
+                semanticsLabel: 'Devfest Logo',
+                width: 29.73.w,
+                height: 16.41.h,
+              ))),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: Constants.horizontalMargin.w),
         child: Column(
           children: [
-            const HeaderText(
-              title: Text('Get attendees checked in faster than before'),
-              subtitle: Text(
-                'Hey rockstar volunteer, we have made attendees check in more convenient for you.',
+            Expanded(
+              child: ListView(
+                children: [
+                  Constants.largeVerticalGutter.verticalSpace,
+                  const HeaderText(
+                    title: Text('Get attendees checked in faster than before'),
+                    subtitle: Text(
+                      'Hey rockstar volunteer, we have made attendees check in more convenient for you.',
+                    ),
+                  ),
+                  SizedBox(height: (Constants.largeVerticalGutter * 3).h),
+                  DevfestFilledButton(
+                    onPressed: () {
+                      context.goNamed(Devfest2024Routes.onboardingLogin.name);
+                    },
+                    title: const Text('Login'),
+                  )
+                ],
               ),
             ),
-            SizedBox(height: (Constants.largeVerticalGutter * 3).h),
-            DevfestFilledButton(
-              onPressed: () {
-                context.goNamed(Devfest2024Routes.onboardingLogin.name);
-              },
-              title: const Text('Login'),
+            Image.asset(
+              'assets/images/onboarding_img1.png',
+              semanticLabel: 'Onboarding Image',
             )
           ],
         ),
