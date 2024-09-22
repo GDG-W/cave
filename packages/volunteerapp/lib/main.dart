610df66fc33459e7fc7982cfc21303fc9fefe65b
Codefarmer <abadasamuelosp@gmail.com>
2024-09-22 15:14:49 +0100

    chore: setup firebase

--- packages/volunteerapp/lib/main.dart
+++ packages/volunteerapp/lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:volunteerapp/volunteer_app.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    const SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+    );
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const VolunteerApp());
 }
