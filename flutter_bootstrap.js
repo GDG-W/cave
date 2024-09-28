6c44e41edfbc3e2570e958e8460d6fddbb952590
DairoIyin <DairoIyin@users.noreply.github.com>
2024-09-28 12:18:25 +0000

    deploy: f4ded4d6e17b7717a116447efd2d7a2f47c51d00

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "785347506"
+    serviceWorkerVersion: "3965001433"
   }
 });
