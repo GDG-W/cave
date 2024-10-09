407664da6ff7bf8c9f630c1f36828bcb28affd9b
Iyinoluwa <dairoiyin@gmail.com>
2024-10-09 17:35:15 +0100

    feat: details screens new

---
+++ packages/cave/lib/ui_utils/container_properties.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ContainerProperties {
+  static var defaultRadius = Radius.circular(8.0.r);
+  static var defaultBorderRadius = BorderRadius.all(defaultRadius);
+
+  static var defaultSecondaryRadius = Radius.circular(4.0.r);
+  static var defaultSecondaryBorderRadius =
+      BorderRadius.all(defaultSecondaryRadius);
+
+  static var defaultPrimaryRadius = Radius.circular(56.0.r);
+  static var defaultPrimaryBorderRadius =
+      BorderRadius.all(defaultPrimaryRadius);
+}
---
+++ packages/conferenceapp/lib/src/shared/widgets/speaker_talk_info_pill.dart
@@ -0,0 +1,29 @@
+import 'package:cave/cave.dart';
+import 'package:flutter/material.dart';
+
+class SpeakersTalkInfoPill extends StatelessWidget {
+  const SpeakersTalkInfoPill(
+      {super.key, required this.icon, required this.title});
+  final IconData icon;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 5.h, horizontal: 12.w),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.r),
+          color: DevfestColors.primariesYellow80),
+      child: Wrap(
+        spacing: 1,
+        children: [
+          Icon(icon),
+          Text(
+            title,
+            style: DevfestTheme.of(context).textTheme?.bodyBody2Medium?.medium,
+          ),
+        ],
+      ),
+    );
+  }
+}
